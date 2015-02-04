23ce01b7e653e056a34221d3413dc859da3e592f
Chris C <chris.c.1221@gmail.com>
2015-02-04 11:37:54 -0500

    comment daemon

--- daemon/teksavvy_cron.py
+++ daemon/teksavvy_cron.py
@@ -1,10 +1,13 @@
 #!/opt/local/bin/python
+#you might have to change the above to point to your local python
  
 from __future__ import division  
 import httplib, json, time
- 
+
+#replace this with your API key 
 APIKEY = "API_KEY"
- 
+
+#pull from api 
 headers = {"TekSavvy-APIKey": APIKEY}
 conn = httplib.HTTPSConnection("api.teksavvy.com")
 conn.request('GET', '/web/Usage/UsageSummaryRecords?$filter=IsCurrent%20eq%20true', '', headers)
@@ -12,7 +15,8 @@ response = conn.getresponse()
 jsonData = response.read()
  
 data = json.loads(jsonData)
- 
+
+#label 
 pd  = data["value"][0]["OnPeakDownload"]
 pu  = data["value"][0]["OnPeakUpload"]
 opd = data["value"][0]["OffPeakDownload"]
@@ -20,13 +24,15 @@ opu = data["value"][0]["OffPeakUpload"]
 sd  = data["value"][0]["StartDate"]
 ed  = data["value"][0]["EndDate"]
 
-#monthly limits:
+#monthly limits: input yours
 monthly_limit = 150
 total_down_used = pd + opd
 
+#take percents
 percentage_of_data_used = total_down_used/monthly_limit * 100
 percentage_of_data_used = round(percentage_of_data_used, 2)
 
+#this could be done better, not sure how
 now = (time.strftime("%x %X"))
 today = time.strftime("%A")
 month = time.strftime("%b")
@@ -41,14 +47,15 @@ percentage_of_month_passed = round(percentage_of_month_passed, 2)
 on_peak_dl_perc = pd/total_down_used*100
 on_peak_dl_perc = round(on_peak_dl_perc, 2)
 
+#send email
 def send_email():
             import smtplib
 
-            gmail_user = "email@gmail.com"
-            gmail_pwd = "pass"
-            FROM = 'email@gmail.com'
+            gmail_user = "email@gmail.com" #change to yours
+            gmail_pwd = "pass" #change to yours
+            FROM = 'email@gmail.com' #change to yours
             TO = ['email@gmail.com'] #must be a list
-            SUBJECT = "Bandwidth Tracking %s %s %s" % (today, month, current_day)
+            SUBJECT = "Bandwidth Tracking %s %s %s" % (today, month, current_day) 
             TEXT = "Good morning! \nToday is %s. \nYou have used %s percent of your data this month, which is equal to %s Gb of data usage. You are %s percent through this month. %s percent of this data is on peak." %(today, percentage_of_data_used, total_down_used, percentage_of_month_passed, on_peak_dl_perc)
             
 #"peak down: %s, peak up: %s, offpeak up: %s, offpeak up: %s" % (pd, pu, opd, opu)
